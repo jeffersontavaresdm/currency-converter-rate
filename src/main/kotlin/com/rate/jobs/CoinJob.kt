bc5a0d5a7f597b7db69fc11bf666576ba9ec5c14
jeffersontdm <jeffersontavares1611@gmail.com>
2021-02-25 11:26:58 -0300

    commit

--- src/main/kotlin/com/rate/jobs/CoinJob.kt
+++ src/main/kotlin/com/rate/jobs/CoinJob.kt
@@ -1,6 +1,9 @@
+@file:Suppress("unused")
+
 package com.rate.jobs
 
 import com.rate.api.AwesomeApi
+import com.rate.entity.Coin
 import com.rate.exception.BadRequestException
 import com.rate.repository.CoinRepository
 import com.rate.service.CoinRateService
@@ -8,12 +11,13 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.OffsetDateTime
 
 @Component
 class CoinJob(
 
-  private val coinRepository: CoinRepository,
   private val coinRateService: CoinRateService,
+  private val coinRepository: CoinRepository,
   private val awesomeApi: AwesomeApi,
 
   ) {
@@ -31,10 +35,35 @@ class CoinJob(
 
       val responseBody = response.body()
         ?: throw BadRequestException(HttpStatus.EXPECTATION_FAILED.toString())
+      val assets = responseBody.values
 
-      responseBody.values.map { asset -> coinRateService.rate(asset.code) }
+      assets.forEach { asset ->
 
+        val now = OffsetDateTime.now()
+        val coinInfoToday =
+          coinRepository.findByTypeAndSavedDate(asset.code, now.toLocalDate())
+            ?: coinRateService
+              .update(
+                Coin(
+                  id = null,
+                  type = asset.code,
+                  name = asset.name,
+                  maxValue = asset.high,
+                  minValue = asset.low,
+                  lastUpdateTime = now.toLocalTime(),
+                  savedDate = now.toLocalDate()
+                )
+              )
+        coinRateService.update(
+          coinInfoToday.copy(
+            maxValue = asset.high,
+            minValue = asset.low,
+            lastUpdateTime = now.toLocalTime(),
+            savedDate = now.toLocalDate()
+          )
+        )
+      }
       logger.info("...updates successful!\n")
-    }
+    } else logger.info(response.errorBody().toString())
   }
 }
--- src/main/kotlin/com/rate/service/CoinRateService.kt
+++ src/main/kotlin/com/rate/service/CoinRateService.kt
@@ -1,58 +1,16 @@
 package com.rate.service
 
-import com.rate.api.AwesomeApi
 import com.rate.entity.Coin
-import com.rate.exception.BadRequestException
 import com.rate.repository.CoinRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import java.time.OffsetDateTime
 
 @Service
-class CoinRateService(
+class CoinRateService(private val coinRepository: CoinRepository) {
 
-  private val coinRepository: CoinRepository,
-  private val awesomeApi: AwesomeApi,
-
-  ) {
-
-  fun rate(coin: String) {
-
-    val response = awesomeApi.makeApiCall(coin).execute()
-
-    if (response.isSuccessful) {
-      val responseBody = response.body()
-        ?: throw BadRequestException(HttpStatus.EXPECTATION_FAILED.toString())
-
-      val assets = responseBody.values
-
-      assets.forEach { asset ->
-
-        val now = OffsetDateTime.now()
-        val coinInfoToday = coinRepository
-          .findByTypeAndSavedDate(asset.code, now.toLocalDate()) ?: coinRepository
-          .save(
-            Coin(
-              id = null,
-              type = asset.code,
-              name = asset.name,
-              maxValue = asset.high,
-              minValue = asset.low,
-              lastUpdateTime = now.toLocalTime(),
-              savedDate = now.toLocalDate()
-            )
-          )
-        coinRepository.save(
-          coinInfoToday.copy(
-            maxValue = asset.high,
-            minValue = asset.low,
-            lastUpdateTime = now.toLocalTime()
-          )
-        )
-      }
-    } else throw BadRequestException(HttpStatus.EXPECTATION_FAILED.toString())
+  fun update(coin: Coin): Coin {
+    return coinRepository.save(coin)
   }
 
   fun currencyValue(coin: String, page: Pageable): Page<Coin> {
