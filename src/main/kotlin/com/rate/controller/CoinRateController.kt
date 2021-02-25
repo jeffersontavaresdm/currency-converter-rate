576a1eeb692741e36c5ab7830f6742ccbd70a69f
jeffersontdm <jeffersontavares1611@gmail.com>
2021-02-25 12:30:21 -0300

    commit

--- src/main/kotlin/com/rate/controller/CoinRateController.kt
+++ src/main/kotlin/com/rate/controller/CoinRateController.kt
@@ -4,6 +4,7 @@ import com.rate.entity.dto.CoinDTO
 import com.rate.service.CoinRateService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 
 @Suppress("unused")
@@ -17,10 +18,15 @@ class CoinRateController(val coinRateService: CoinRateService) {
     @PathVariable("coinType") coinType: String,
     @RequestParam("page") page: Int,
     @RequestParam("pageSize") pageSize: Int,
+//    pageable: Pageable,
 
     ): Page<CoinDTO> {
 
-    val infoList = coinRateService.currencyValue(coinType.toUpperCase(), PageRequest.of(page, pageSize))
+    val infoList = coinRateService.currencyValue(
+      coin = coinType.toUpperCase(),
+      pageable = PageRequest.of(page, pageSize)
+//      pageable = pageable
+    )
 
     return infoList.map { coin ->
       CoinDTO(
