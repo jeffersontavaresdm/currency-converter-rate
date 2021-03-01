bc24ba6a943149eee9ab283ac10e7f17f36ed390
jeffersontdm <jeffersontavares1611@gmail.com>
2021-03-01 16:36:33 -0300

    commit

--- src/main/resources/db/migration/V20210219084217__populate_sample_currency_table.sql
+++ src/main/resources/db/migration/V20210219084217__populate_sample_currency_table.sql
@@ -1,14 +1,14 @@
-INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
-VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-24');
-
-INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
-VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-23');
-
-INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
-VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-22');
-
-INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
-VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-21');
-
-INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
-VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-20');
+--INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
+--VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-24');
+--
+--INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
+--VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-23');
+--
+--INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
+--VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-22');
+--
+--INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
+--VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-21');
+--
+--INSERT INTO currency (type, name, max_value, min_value, last_update_time, saved_date)
+--VALUES ('USD', 'Dólar Comercial', '5.4270', '5.4272', '00:00-00', '2021-02-20');
