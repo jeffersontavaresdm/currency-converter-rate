7120d67573f7cd1e9c809a9bb5d1ead0d170f731
Jefferson Tavares <44708671+jeffersontavaresdm@users.noreply.github.com>
2022-01-10 20:34:36 -0300

    small changes

--- src/main/kotlin/com/rate/service/CurrencyConverterService.kt
+++ src/main/kotlin/com/rate/service/CurrencyConverterService.kt
@@ -4,12 +4,15 @@ import com.rate.entity.Currency
 import com.rate.entity.dto.CurrencyDTO
 import com.rate.entity.dto.toDTO
 import com.rate.repository.CurrencyRepository
+import com.rate.utils.loggerFor
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class CurrencyConverterService(private val repository: CurrencyRepository) {
+
+  private val logger = loggerFor()
 
   fun save(currency: Currency): Currency {
     return repository.save(currency)
@@ -21,7 +24,10 @@ class CurrencyConverterService(private val repository: CurrencyRepository) {
     return if (currencyType == "all") {
       repository
         .findAll(pageable)
-        .map { it.toDTO() }
+        .map { currency ->
+          logger.info("Currency {}: ", currency)
+          currency?.toDTO()
+        }
     } else {
       page.map { it.toDTO() }
     }
