ef1cd3b1c1d04b402b059d7e0763c1d5f6c9fba5
Jefferson Tavares <44708671+jeffersontavaresdm@users.noreply.github.com>
2022-01-11 12:24:26 -0300

    small changes

--- src/main/kotlin/com/rate/service/CurrencyConverterService.kt
+++ src/main/kotlin/com/rate/service/CurrencyConverterService.kt
@@ -4,7 +4,6 @@ import com.rate.entity.Currency
 import com.rate.entity.dto.CurrencyDTO
 import com.rate.entity.dto.toDTO
 import com.rate.repository.CurrencyRepository
-import com.rate.utils.loggerFor
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -12,27 +11,23 @@ import org.springframework.stereotype.Service
 @Service
 class CurrencyConverterService(private val repository: CurrencyRepository) {
 
-  private val logger = loggerFor()
-
   fun save(currency: Currency): Currency {
     return repository.save(currency)
   }
 
   fun getInfoList(type: String, pageable: Pageable): Page<CurrencyDTO> {
-    val page = repository.findAllByTypeOrderBySavedDateDesc(type, pageable)
-
-    return page.map { currency ->
-      logger.info("Currency:\n{}", currency)
-      currency.toDTO()
-    }
+    return repository
+      .findAllByTypeOrderBySavedDateDesc(type, pageable)
+      .map { it.toDTO() }
   }
 
   fun getAll(): MutableMap<String, CurrencyDTO> {
-    val map = mutableMapOf<String, CurrencyDTO>()
-    repository.getAllOrderById().forEach { currency ->
-      logger.info("Currency:\n{}", currency)
-      map[currency.type] = currency.toDTO()
-    }
-    return map
+    return mutableMapOf<String, CurrencyDTO>()
+      .apply {
+        repository.getAllOrderById()
+          .forEach {
+            this[it.type] = it.toDTO()
+          }
+      }
   }
 }
